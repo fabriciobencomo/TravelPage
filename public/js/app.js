32f3d755da4c27ffa9e37efe719b151f226c216e
Fabricio Bencomo <Fabriciobencomo.viloria@gmail.com>
2022-03-04 01:13:10 -0400

    javascript toggle for dark mode and minify tailwindcss

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+const htmlElement = document.querySelector('html');
+const toggleButton = document.querySelectorAll('.toggle');
+toggleButton.forEach( item => {
+    item.addEventListener('click', () => {
+        htmlElement.classList.toggle( 'dark' );
+    });
+});
